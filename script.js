fde71065ef0306931c2fdc9bb0f1673132cc4f83
Fernandonar <43646415+Fernandonar@users.noreply.github.com>
2021-11-18 08:57:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,157 @@
+$(document).ready(() => {
+
+
+	
+	let $userName = "Fer";
+
+	
+	$("#form-start").on("submit", (event) => {
+		event.preventDefault();
+		$userName = $("#username").val();
+		$("#landing").slideUp(300);
+		setTimeout(() => {
+			$("#start-chat").html("Continue chat")
+		}, 300);
+	});
+
+
+
+
+	function $postMessage() {
+		$("#message").find("br").remove();
+		let $message = $("#message").html().trim(); 
+		$message = $message.replace(/<div>/, "<br>").replace(/<div>/g, "").replace(/<\/div>/g, "<br>").replace(/<br>/g, " ").trim();
+		if ($message) { 
+			const html = `<div class="post post-user">${$message + timeStamp()}</span></div>`; 
+			$("#message-board").append(html); 
+			$scrollDown(); 
+			botReply($message);
+		};
+		$("#message").empty();
+	};
+
+	// Chat input
+	$("#message").on("keyup", (event) => {
+		if (event.which === 13) $postMessage();
+	}).on("focus", () => {
+		$("#message").addClass("focus");
+	}).on("blur", () => {
+		$("#message").removeClass("focus");
+	});
+	$("#send").on("click", $postMessage);
+
+
+
+
+
+	function botReply(userMessage) {
+		const reply = generateReply(userMessage);
+		if (typeof reply === "string") postBotReply(reply);
+		else reply.forEach((str) => postBotReply(str));
+	};
+
+	function generateReply(userMessage) {
+		const message = userMessage.toLowerCase();
+		let reply = [`No te entiendo`];
+
+		// Generate some different replies
+		if (/^hi$|^hola|^howdy|^hoi|^hey|^ola/.test(message)) reply = [`Hola ${$userName}`, `Que puedo hacer por ti?`];
+		else if (/prueba/.test(message)) reply = [`Ok`, `puedes probar este chat`];
+		else if (/ayuda|sos|emergency|support/.test(message)) reply = [`En que puedo ayudar?`, `What seems to be the problem?`];
+		else if (/class\=\"fa/.test(message)) reply = [`I see you've found the smileys`, `Cool! <span class="far fa-grin-beam fa-2x"></span>`, `Did you need something?`];
+		else if (/how|what|why/.test(message)) reply = userMessage + " what?";
+		else if (/^huh+|boring|lame|wtf|pff/.test(message)) reply = [`<span class="far fa-dizzy fa-2x"></span>`, `I'm sorry you feel that way`, `How can I make it better?`];
+		else if (/bye|ciao|adieu|salu/.test(message)) reply = [`Ok, bye :)`];
+
+		return reply;
+	};
+
+	function postBotReply(reply) {
+		const html = `<div class="post post-bot">${reply + timeStamp()}</div>`;
+		const timeTyping = 500 + Math.floor(Math.random() * 2000);
+		$("#message-board").append(html);
+		$scrollDown();
+	};
+
+
+
+	/******************/
+	/*** TIMESTAMPS ***/
+	/******************/
+
+
+	function timeStamp() {
+		const timestamp = new Date();
+		const hours = timestamp.getHours();
+		let minutes = timestamp.getMinutes();
+		if (minutes < 10) minutes = "0" + minutes;
+		const html = `<span class="timestamp">${hours}:${minutes}</span>`;
+		return html;
+	};
+
+
+
+
+	/***************/
+	/*** CHAT UI ***/
+	/***************/
+
+
+	// Back arrow button
+	$("#back-button").on("click", () => {
+		$("#landing").show();
+	});
+
+
+	// Menu - navigation
+	$("#nav-icon").on("click", () => {
+		$("#nav-container").show();
+	});
+
+	$("#nav-container").on("mouseleave", () => {
+		$("#nav-container").hide();
+	});
+
+	$(".nav-link").on("click", () => {
+		$("#nav-container").slideToggle(200);
+	});
+
+	// Clear history
+	$("#clear-history").on("click", () => {
+		$("#message-board").empty();
+		$("#message").empty();
+	});
+
+	// Sign out
+	$("#sign-out").on("click", () => {
+		$("#message-board").empty();
+		$("#message").empty();
+		$("#landing").show();
+		$("#username").val("");
+		$("#start-chat").html("Start chat");
+	});
+
+
+
+
+	/*********************/
+	/*** SCROLL TO END ***/
+	/*********************/
+
+
+	function $scrollDown() {
+		const $container = $("#message-board");
+		const $maxHeight = $container.height();
+		const $scrollHeight = $container[0].scrollHeight;
+		if ($scrollHeight > $maxHeight) $container.scrollTop($scrollHeight);
+	}
+
+
+
+
+
+
+
+
+
+});
